badab09f6004817e8e0a2b25fa942fa882a22aaf
DiNitride <j.ptb.t.ca@gmail.com>
2022-04-19 21:17:29 +0100

    Feedback reviewing

--- pages/api/feedback/[id].js
+++ pages/api/feedback/[id].js
@@ -1,11 +1,16 @@
 import api from '../../../lib/api'
-import { selectFeedbackById } from '../../../lib/feedback'
+import { reviewFeedback, selectFeedbackById } from '../../../lib/feedback'
 
 export default api({
   'GET': {
     authenticated: true,
     roles: ['staff'],
     handler: get
+  },
+  'POST': {
+    authenticated: true,
+    roles: ['staff'],
+    handler: post
   }
 })
 
@@ -13,4 +18,15 @@ async function get(req, res, session) {
   let { id } = req.query
   let feedback = await selectFeedbackById(id)
   res.json(feedback)
+}
+
+async function post(req, res, session) {
+  let { id } = req.query
+  let { action } = req.body
+  if (action === 'review') {
+    await reviewFeedback(id, session.user.UserId)
+    res.json({ success: true })
+  } else {
+    res.status(400).json({ error: 'Invalid post action' })
+  }
 }
--- pages/dashboard/feedback.js
+++ pages/dashboard/feedback.js
@@ -8,7 +8,20 @@ import Modal from '../../components/Modal'
 import CreateReferralForm from "../../components/CreateReferralForm"
 
 export default function Donations({}) {
-  let { data, error } = useSWR('/api/feedback')
+  let { data, error, mutate } = useSWR('/api/feedback')
+
+  let handleReview = async (id) => {
+    let r = await fetch(`/api/feedback/${id}`, {
+      method: 'POST',
+      body: JSON.stringify({ action: 'review' }),
+      headers: {'Content-Type': 'application/json'}
+    })
+    if (!r.ok) {
+      let { error } = await r.json()
+      alert(error)
+    }
+    mutate()
+  }
 
   return (
     <DashboardLayout>
@@ -21,8 +34,10 @@ export default function Donations({}) {
         <table className='border border-collapse table-fixed'>
           <thead>
             <tr>
-              <th className='border'>Feedback</th>
-              <th className='border'>Recieved</th>
+              <th className='border p-1'>Feedback</th>
+              <th className='border p-1'>Recieved</th>
+              <th className='border p-1'>Reviewed</th>
+              <th className='border p-1 whitespace-nowrap'>Reviewed By</th>
             </tr>
           </thead>
           <tbody>
@@ -31,6 +46,8 @@ export default function Donations({}) {
               return <tr key={feedback.FeedbackId}>
                 <td className='border p-2 whitespace-pre w-full'>{ feedback.FeedbackText }</td>
                 <td className='border p-2 text-center w-auto whitespace-nowrap'>{ feedback.PrettyFeedbackRecieved }</td>
+                <td className='border p-2 text-center w-auto whitespace-nowrap cursor-pointer' onClick={() => handleReview(feedback.FeedbackId)}><FontAwesomeIcon icon={feedback.FeedbackReviewed ? 'check' : 'xmark'} /></td>
+                <td className='border p-2 text-center w-auto whitespace-nowrap'>{ feedback.UserForename } {feedback.UserSurname }</td>
               </tr>
             }) : <tr><td colSpan={5} className="text-center border">No donations</td></tr> }
           </tbody>
