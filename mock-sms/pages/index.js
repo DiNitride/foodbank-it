b9ec46dbd4f65a949b441e855602d830578840bf
DiNitride <j.ptb.t.ca@gmail.com>
2022-04-26 10:20:52 +0100

    update mock index

--- mock-sms/pages/index.js
+++ mock-sms/pages/index.js
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <div>
       <h1>SMS Numbers</h1>
-      <p><a href='/07333222111'>07888222111 - Jamie Roberts</a></p>
+      <p><a href='/07888222111'>07888222111 - Jamie Roberts</a></p>
       <p><a href='/07111222333'>07111222333 - Samantha May</a></p>
       <p><a href='/07666555444'>07666555444 - Alex Milan</a></p>
-      <p><a href='/07999333222'>07999333222 - Supplier Store Applicant Number (John Doe)</a></p>
-      <p><a href='/07888999222'>07888999222 - Social Service Applicant Number (Richard Michaels)</a></p>
+      <p><a href='/07999333222'>07999333222 - Supplier Store Contact (John Doe)</a></p>
+      <p><a href='/07888999222'>07888999222 - Social Service Contact (Richard Michaels)</a></p>
     </div>
   )
 }
