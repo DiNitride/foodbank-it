ae5a42dd9f1f004f7efa61921471cfa241e7a4da
DiNitride <j.ptb.t.ca@gmail.com>
2022-05-16 12:52:01 +0100

    Add input to mock sms

--- mock-sms/pages/index.js
+++ mock-sms/pages/index.js
@@ -1,9 +1,19 @@
+import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
 
 export default function Home() {
+  let router = useRouter()
+  let [number, setNumber] = useState('')
   return (
     <div>
       <h1>SMS Numbers</h1>
+      <input
+        placeholder='Enter Number'
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      <button onClick={() => router.push('/' + number)}>Go</button>
+      <h2>Quick Links</h2>
       <p><a href='/07888222111'>07888222111 - Jamie Roberts</a></p>
       <p><a href='/07111222333'>07111222333 - Samantha May</a></p>
       <p><a href='/07666555444'>07666555444 - Alex Milan</a></p>
